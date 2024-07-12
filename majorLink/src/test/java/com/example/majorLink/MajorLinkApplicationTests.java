a6315e7b87b3d115e7d8fcf7a132a46ab47d9b54
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2024-07-12 13:59:02 +0900

    프로젝트 생성, rds 연결

---
+++ majorLink/src/test/java/com/example/majorLink/MajorLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.majorLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajorLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
