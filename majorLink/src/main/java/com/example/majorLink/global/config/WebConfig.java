2cacfd2f50be9f15ec5ec24d257f6951c9e43284
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2024-08-06 00:38:22 +0900

    webConfig 작성

--- majorLink/src/main/java/com/example/majorLink/global/config/WebConfig.java
+++ majorLink/src/main/java/com/example/majorLink/global/config/WebConfig.java
@@ -1,4 +1,18 @@
 package com.example.majorLink.global.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("**/")
+                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3001") //프론트 테스트 url
+                .allowedMethods("POST", "GET", "DELETE", "PUT")
+                .allowedHeaders("*");
+    }
 }
