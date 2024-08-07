8a7b013dacac62659025c1eb412b802999485eee
kchaeeun <oceanball38@naver.com>
2024-08-08 01:12:56 +0900

    ✨ [FEAT] 마이 페이지 조회 API

---
+++ majorLink/src/main/java/com/example/majorLink/dto/response/ProfileResponse.java
@@ -0,0 +1,20 @@
+package com.example.majorLink.dto.response;
+
+import com.example.majorLink.domain.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class ProfileResponse {
+    private String profileImg;
+    private String username;
+    private String email;
+    private String firstMajor;
+    private String secondMajor;
+    private String favorite;
+    private Gender gender;
+    private String phone;
+}
--- majorLink/src/main/java/com/example/majorLink/service/UserService.java
+++ majorLink/src/main/java/com/example/majorLink/service/UserService.java
@@ -1,10 +1,14 @@
 package com.example.majorLink.service;
 
+import com.example.majorLink.domain.User;
 import com.example.majorLink.dto.request.SignInRequest;
 import com.example.majorLink.dto.request.SignUpRequest;
+import com.example.majorLink.dto.response.ProfileResponse;
 import com.example.majorLink.global.auth.Tokens;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface UserService{
     Tokens signIn(SignInRequest request);
     Tokens signUp(SignUpRequest request);
+    ProfileResponse getProfile(User user);
 }
