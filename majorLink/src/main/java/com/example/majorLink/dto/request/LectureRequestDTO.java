2b6c431ebad95ea8a8a614f5f04bceb74b0815f4
Chaerin Ryu <dmsquf1009@dgu.ac.kr>
2024-08-17 00:31:19 +0900

    :zap: Feat: 강의 생성, 수정, 삭제 기능 리팩토링

--- majorLink/src/main/java/com/example/majorLink/dto/request/LectureRequestDTO.java
+++ majorLink/src/main/java/com/example/majorLink/dto/request/LectureRequestDTO.java
@@ -1,7 +1,5 @@
 package com.example.majorLink.dto.request;
 
-import com.example.majorLink.domain.Category;
-import com.example.majorLink.domain.Tag;
 import com.example.majorLink.domain.enums.Day;
 import com.example.majorLink.domain.enums.Exam;
 import com.example.majorLink.domain.enums.Level;
@@ -15,17 +13,16 @@ import java.util.UUID;
 public class LectureRequestDTO {
     // 새로운 강의 등록할때 필요한 정보들 작성
     String name;
-    String body;
-    int curri;
-    String info;
+    String body; // 강의 설명
+    int curri; // 강의 횟수
+    String info; // 강의 정보
     Level level;
     int pNum;
     LocalTime time;
-    Day day;
+    Day day; // 요일
     Date startDate;
-    Exam exam;
+    Exam exam; // 시험 유무
     Long categoryId;
-    Long tagId;
-    UUID userId;
-
+    String tag;
+    UUID userId; // 강의 생성자
 }
--- majorLink/src/main/java/com/example/majorLink/service/LectureService.java
+++ majorLink/src/main/java/com/example/majorLink/service/LectureService.java
@@ -1,14 +1,26 @@
 package com.example.majorLink.service;
 
 import com.example.majorLink.domain.Lecture;
-import com.example.majorLink.domain.User;
+import com.example.majorLink.domain.mapping.TuteeLecture;
 import com.example.majorLink.dto.request.LectureRequestDTO;
 import org.springframework.data.domain.Page;
 
+import java.util.UUID;
+
 public interface LectureService {
-    Lecture createLecture(User user, LectureRequestDTO request);
-    Lecture updateLecture(User user, Long lectureId, LectureRequestDTO request);
-    void DeleteLecture(User user, Long lectureId);
+    Lecture createLecture(UUID userId, LectureRequestDTO request);
+    Lecture updateLecture(UUID userId, LectureRequestDTO request, Long lectureId);
+    void DeleteLecture(UUID userId, Long lectureId);
 
     Page<Lecture> getLectureList(Integer page);
+
+    TuteeLecture addLecture(UUID userId, Long lectureId);
+    void cancelLecture(UUID userId, Long lectureId);
+
+    Boolean toggleLike(UUID userId, Long lectureId);
+
+    Page<Lecture> getMostLikedLecture(Integer page);
+    Page<Lecture> getNewLecture(Integer page);
+    Page<Lecture> getMostRecruitedLecture(Integer page);
+    Page<Lecture> getLectureByCategory(Integer page, Long categoryId);
 }
