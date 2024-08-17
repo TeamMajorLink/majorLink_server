a3aa238c452b335769b2e8e9c8a4127cabd8aad8
kchaeeun <oceanball38@naver.com>
2024-08-17 22:50:37 +0900

    ✨ [FEAT] API 연동을 위한 providerId, accessToken 처리

--- majorLink/src/main/java/com/example/majorLink/global/oauth2/OAuthAttributes.java
+++ majorLink/src/main/java/com/example/majorLink/global/oauth2/OAuthAttributes.java
@@ -23,6 +23,11 @@ public class OAuthAttributes {
     private String profileImg;
     private Gender gender;
     private String phone;
+    private String accessToken;
+
+    public void updateAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
 
     public static OAuthAttributes of(SocialType provider, String userNameAttribute, Map<String, Object> attributes) {
         Map<String, Object> response = (Map<String, Object>) attributes.get(userNameAttribute);
@@ -36,18 +41,19 @@ public class OAuthAttributes {
 
     public static OAuthAttributes ofNaver(Map<String, Object> response) {
         OAuthAttributes oAuthAttributes = new OAuthAttributes();
-        oAuthAttributes.id = (String) response.get("id");
-        oAuthAttributes.email = (String) response.get("email");
-        oAuthAttributes.username = (String) response.get("name");
-        oAuthAttributes.phone = (String) response.get("phone");
-        oAuthAttributes.profileImg = (String) response.get("profile_image");
-        String gender = (String) response.get("gender");
-
-        if ("F".equals(gender)) {
-            oAuthAttributes.gender = Gender.FEMALE;
-        } else if ("M".equals(gender)) {
-            oAuthAttributes.gender = Gender.MALE;
-        }
+//        oAuthAttributes.id = (String) response.get("id");
+//        oAuthAttributes.email = (String) response.get("email");
+//        oAuthAttributes.username = (String) response.get("name");
+//        oAuthAttributes.phone = (String) response.get("phone");
+//        oAuthAttributes.profileImg = (String) response.get("profile_image");
+        oAuthAttributes.accessToken = (String) response.get("accessToken");
+//        String gender = (String) response.get("gender");
+//
+//        if ("F".equals(gender)) {
+//            oAuthAttributes.gender = Gender.FEMALE;
+//        } else if ("M".equals(gender)) {
+//            oAuthAttributes.gender = Gender.MALE;
+//        }
 
         return oAuthAttributes;
     }
