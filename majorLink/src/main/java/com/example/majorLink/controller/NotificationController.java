d98da0d3421741b4f60b2b4b4ce5430a21512a0b
kchaeeun <oceanball38@naver.com>
2024-08-19 17:47:24 +0900

    💡 [ADD] 컨트롤러 기능 주석 추가

--- majorLink/src/main/java/com/example/majorLink/controller/NotificationController.java
+++ majorLink/src/main/java/com/example/majorLink/controller/NotificationController.java
@@ -20,6 +20,14 @@ public class NotificationController {
     private final NotificationService notificationService;
 
     // Last-Event-ID는 SSE 연결이 끊어졌을 경우, 클라이언트가 수신한 마지막 데이터 ID 값을 의미, 항상 존재 X -> false
+
+    /**
+     * 알림을 위한 구독 API
+     * [GET] /notification/subscribe
+     * @param authUser
+     * @param lastEventId
+     * @return
+     */
     @GetMapping(value = "/subscribe", produces = "text/event-stream")
     public SseEmitter subscribe(
             @AuthenticationPrincipal AuthUser authUser,
@@ -28,6 +36,12 @@ public class NotificationController {
         return notificationService.subscribe(user, lastEventId);
     }
 
+    /**
+     * 알림 전체 조회
+     * [GET] /notification
+     * @param authUser
+     * @return
+     */
     @GetMapping
     public ResponseEntity<List<NotificationResponse>> getNotificationList(
             @AuthenticationPrincipal AuthUser authUser) {
