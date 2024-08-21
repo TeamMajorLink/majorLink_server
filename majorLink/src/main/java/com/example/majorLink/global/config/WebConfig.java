99d79b082e4524f8b962b911f5353b91e7351b1c
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2024-08-21 09:55:39 +0900

    x-auth-token 허용헤더에 추가

--- majorLink/src/main/java/com/example/majorLink/global/config/WebConfig.java
+++ majorLink/src/main/java/com/example/majorLink/global/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
                         "http://localhost:3004"
                 ) //프론트 테스트 url입니다.
                 .allowedMethods("POST", "GET", "DELETE", "PUT", "PATCH", "OPTIONS")
-                .allowedHeaders("Content-Type", "Authorization")
+                .allowedHeaders("Content-Type", "Authorization","x-auth-token")
                 .allowCredentials(true);
     }
 }
